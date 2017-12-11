6ded59f8f62bbe5a98e834f5fdc6a8b4cfc7d916
Dillon Lomnitzer <yodaesu@gmail.com>
2017-12-11 11:57:54 -0500

    Add files via upload
    Cookies, About, Clean-up

---
+++ about.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Simple Weather</title>
+        <link href="assets/css/style.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script> 
+        <script src="https://use.fontawesome.com/a839cd93dd.js"></script>
+    </head>
+    <body>
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12 navigation-col">
+                    <nav class="navbar navbar-light justify-content-between">
+                        <a class="navbar-brand" href="/">Stupid Simple Weather <i class="fa fa-cloud" aria-hidden="true"></i></a>
+                        <a class="nav-item nav-link active" href="#">About</a>
+                    </nav>
+                </div>
+            </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="article">
+                        <h2 id="about-title">About Stupid Simple Weather</h2>
+                        <p id="small-title">So simple, its stupid!</p>
+                        <hr>
+                        <p style="text-align:center; margin-top:2rem;">No Ads, No distractions, Just Weather.</p>
+                        <p>SSW is a personal project created with the intention of displaying a simple weather solution for anyone tired of visiting a site with 90% Ads and 10% weather. We 
+                            don't want your money, and we definitely do not want to waste your time with videos and advertisements. Just enjoy our simple weather.</p>
+                        <p>SSW is built with Weather Underground's API, so you know the weather you are receiving is trustworthy and in real time.</p>
+                        <p>A couple things to note about this application:</p>
+                        <ul>
+                            <li>This site is still a work in progress.</li>
+                            <li>We will not accepct any solicitations.</li>
+                            <li>SSW is an open source project, anyone who is willing to help out will receive an excellent high five and a thank you.</li>
+                                <ul>
+                                    <li>Source code available here: <a href="https://github.com/DLzer/Stupid-Simple-Weather" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a></li>
+                                </ul>
+                        </ul>
+                        <hr>
+                        <img src="assets/images/wulogo.png" height="45">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </body>
+</html>
--- index.php
+++ index.php
@@ -7,26 +7,31 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script> 
+        <script src="https://use.fontawesome.com/a839cd93dd.js"></script>
     </head>
     <body>
 
-        <div class="container">
+    
+    <div class="wrapper">
+        <div class="container-fluid">
             <div class="row">
-                <div class="col-md-12">
-                    <h1 id="site-title">Stupid Simple Weather</h1>
+                <div class="col-md-12 navigation-col">
+
+                    <nav class="navbar navbar-light justify-content-between">
+                        <a class="navbar-brand" href="/">Stupid Simple Weather <i class="fa fa-cloud" aria-hidden="true"></i></a>
+                        <a class="nav-item nav-link" href="about.php">About</a>
+                        <form class="form-inline">
+                            <input class="form-control mr-sm-2" id="zip-search" type="search" placeholder="ex. 10940" aria-label="Search">
+                            <button id="zip-submit" class="btn btn-outline-success my-2 my-sm-0" name="zip-submit" type="submit">Search</button>
+                        </form>
+                    </nav>
+                    
                 </div>
             </div>
             <div class="row justify-content-md-center">
                 <div class="col-md-6">
                     <div class="warning">
                     </div>
-                    <form>
-                        <div class="form-group">
-                            <input type="text" class="form-control" id="zip-search" name="zip-search" aria-describedby="zipHelp" placeholder="Enter ZIP Code">
-                            <small id="zipHelp" class="form-text text-muted">ex. 10940</small>
-                        </div>
-                        <button id="zip-submit" type="submit" name="zip-submit" class="btn btn-primary">Submit</button>
-                    </form>
                 </div>
             </div>
         </div>
@@ -40,6 +45,17 @@
             </div>
         </div>
 
+        <div class="push"></div>
+    </div>
+
+        <footer class="footer">
+            <div class="container">
+                <span class="text-muted footer-text">API Data courtesy of </span><span><img src="assets/images/wulogo.png" height="30px"/></span>
+            </div>
+        </footer>
+
+
+        <script type="text/javascript" src="assets/js/js.cookie.js"></script>
         <script type="text/javascript" src="assets/js/weather.js"></script>
     </body>
 </html>
