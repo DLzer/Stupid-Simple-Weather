3c5864db3feb0a44d7bde3302b339eb143735c45
Dillon Lomnitzer <yodaesu@gmail.com>
2018-05-12 22:22:11 -0400

    es6 refactor and format

--- js/weather.js
+++ js/weather.js
@@ -1,169 +1,193 @@
-$(document).ready(function(){
-    
-    $('.noLocation').hide();
-    
-    // If HTML5 GeoLocate is available use it for LatLong, then reverse geoCode with getZip().
-    // If HTML5 GeoLocate is not available, use Google GeoLocation API for the LatLong.
-    // If neither can access the user location display an error message.
-    if("geolocation" in navigator) {
-        console.log('Geolocation Available');
-        navigator.geolocation.getCurrentPosition(function(position){
-            var latLong = position.coords.latitude+','+position.coords.longitude;
-            getZip(latLong);
-        });
-    } else if(tryGeoLocation()) {
-        tryGeoLocation()
-    } else {
-        $('.noLocation').show();
-    }
+const WXURL = 'https://api.wunderground.com/';
+const GEOURL = 'https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCMxoo103kh48YCL8VFbCc5aY5-Zn9C_xk';
+let weather = document.querySelector('.weather');
+let yourLocation = document.querySelector('.location');
+let weatherBoxContainer = document.querySelector('.weather-box-container');
+let zipCodePattern = /^\d{5}$|^\d{5}-\d{4}$/;
+let form = document.getElementById("zipForm"); 
 
-    $('#zip-submit').on('click', function(e){
-        e.preventDefault();
-        var zipCode = $('#zip-search').val();
 
-            if($('.weather-row')) {
-                $('.weather-row').children().remove();
-            }
-            if($('#intro')) {
-                $('#intro').remove();
-            }
-            weatherCall(zipCode);
-    })
-
-    dateAndTime = () => {
-        var $date = $('.date');
-        var $clock = $('.clock');
-
-        var months = [
-            'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
-          ];
-          var days = [
-            'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
-          ];
-
-        var date = new Date();
-        
-        var ampm = date.getHours() < 12 
-                    ? 'AM' : 'PM';
-        
-        var hours = date.getHours() == 0
-                    ? 12
-                    : date.getHours() > 12
-                    ? date.getHours() - 12
-                    : date.getHours();
-        
-        var minutes = date.getMinutes() < 10 
-                    ? '0' + date.getMinutes() 
-                    : date.getMinutes();
-        
-        var seconds = date.getSeconds() < 10 
-                    ? '0' + date.getSeconds() 
-                    : date.getSeconds();
-        var dayOfWeek = days[date.getDay()];
-        var month = months[date.getMonth()];
-        var day = date.getDate();
-        var year = date.getFullYear();
-
-        var dateString = dayOfWeek + ', ' + month + ' ' + day + ', ' + year;
-        var clockString = hours + ':' + minutes + ':' + seconds +' '+ ampm;
-
-        $date.text(dateString);
-        $clock.text(clockString);
-    }
-    dateAndTime();
-    setInterval(dateAndTime, 1000);
-
+// Ready, steady, locate.
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Animated Weather Icons by: Tuan Hoang - https://codepen.io/code4food/pen/rLvggd');
+    // SSL ONLY
+    //if(!navigator.geolocation) {
+        geoLocate();
+    // } else {
+    //     console.log("[Geolocation Available]");
+    //     let lat = position.coords.latitude;
+    //     let long = position.coords.longitude;
+    //     let URL = `${WXURL}/api/9dec8582b507efb5/geolookup/forecast/q/${lat},${long}.json`;
+    //     getWeather(URL);
+    // }
 });
 
-weatherCall = (zipCode) => {
-                // Small validation for zip code
-                if(zipCode.length === 5) {
-                    // Call to weather
-                        $.ajax({
-                            url: 'http://api.wunderground.com/api/9dec8582b507efb5/forecast/q/'+zipCode +'.json',
-                            method: 'POST',
-                            data: {},
-                            success: function(data) {
-                                // Add a title with the zip code.
-                                $('.weather-title').append('<h2 id="intro">Your Stupid Simple Weather for '+zipCode+'..</h2>');
-                                // Loop through the forecasts and create a card for each one.
-                                for(var i = 0, len = data.forecast.simpleforecast.forecastday.length; i < len; i++) {
-                                var weatherCard = '<div class="col-sm-3 weather-card">'+
-                                                  '<div class="card">'+
-                                                  '<div class="card-body">'+
-                                                  '<h4 class="card-title title-day">'+data.forecast.simpleforecast.forecastday[i].date.weekday+'</h4>'+
-                                                  '<div class="icon-day"><img id="icon-day" src='+data.forecast.simpleforecast.forecastday[i].icon_url+'></div>'+
-                                                  '<h6 class="card-subtitle mb-2 text-muted icon">'+data.forecast.simpleforecast.forecastday[i].conditions+'</h6>'+
-                                                  '<p class="card-text forecast">'+data.forecast.simpleforecast.forecastday[i].high.fahrenheit+' - '+data.forecast.simpleforecast.forecastday[i].low.fahrenheit+'</p>'+
-                                                  '<p class="down-arrow"><i class="fa fa-chevron-down" aria-hidden="true"></i></p>'+
-                                                  '</div>'+
-                                                  '</div>'+
-                                                  '</div>';
-                                // Append the cards to DOM.
-                                $('.weather-row').append(weatherCard);
-                                }
-                                // Loop through the nightly forecasts and create a card for each one.
-                                for(var j = 1, jlen = data.forecast.txt_forecast.forecastday.length; j < jlen; j+=2) {
-                                    var nightWeather = '<div class="col-sm-3 nightCard">'+
-                                                       '<div class="card">'+
-                                                       '<div class="card-body nightCard-body">'+
-                                                       '<h4 class="card-title title-day">'+data.forecast.txt_forecast.forecastday[j].title+'</h4>'+
-                                                       '<div class="icon-day"><img id="icon-day" src="'+data.forecast.txt_forecast.forecastday[j].icon_url+'"></div>'+
-                                                       '<p class="card-text night-forecast">'+data.forecast.txt_forecast.forecastday[j].fcttext+'</p>'+
-                                                       '</div>'+
-                                                       '</div>'+
-                                                       '</div>';
-                                    // Append the cards to DOM.
-                                    $('.weather-row').append(nightWeather);
-                                }
-                                // Initially hide the night forecast cards
-                                $('.nightCard').hide();
-                                // Click on the down arrow to toggle displaying the night cards.
-                                $('.down-arrow').on('click',function(){
-                                    $(this).find('i').toggleClass('fa-chevron-down fa-chevron-up');
-                                    $('.nightCard').fadeToggle();
-                                })
-                            }
-                        })
-            
-                    } else {
-                        // Alert variable for vaildation failure
-                        var syntaxAlert = '<div class="alert alert-warning" role="alert">' +
-                                          'The zip code you entered was incorrect. Please try again!' +
-                                          '</div>';
-                            // Validation failure, display alert for 10 seconds.
-                            $('.warning').append(syntaxAlert);
-                            window.setTimeout(function(){
-                                $('.alert').remove();
-                            }, 10000);
-                    }
-}
+// Prevent form from firing
+function handleForm(event) { 
+    event.preventDefault(); 
+} 
+form.addEventListener('submit', handleForm);
 
-tryGeoLocation = () => {
-    $.ajax({
-        type: 'POST',
-        url: 'https://www.googleapis.com/geolocation/v1/geolocate?key=YOUR_API_KEY',
-        data: {},
-        success: function(data){
-            var latLong = data.location.lat + ',' + data.location.lng;
-            getZip(latLong);
-            return true;
-        }
-    })
-}
+// Hand the manual search for a ZIP code.
+function manualSearch() {
+    // Remove all current weather nodes.
+    while (weatherBoxContainer.hasChildNodes()) {   
+        weatherBoxContainer.removeChild(weatherBoxContainer.firstChild);
+    }
 
-getZip = (latLong) => {
-    var apiKey = 'YOUR_API_KEY';
-    $.ajax({
-        type: 'POST',
-        url: 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+latLong+'&key='+apiKey+'',
-        data: {},
-        success: function(data){
-            let postalCode = data.results[0].address_components.find(function (component) {
-                return component.types[0] == "postal_code";
-            });
-            var geoZip = postalCode.long_name;
-            weatherCall(geoZip);
-        }
-    })
+    var zipValue = document.getElementById('zipsearch').value;
+
+    // Validate the ZIP code using a regular expression, if it password get the weather.
+    if(zipCodePattern.test(zipValue)) {
+        const ZIP = zipValue;
+        const MANURL = `${WXURL}/api/9dec8582b507efb5/geolookup/forecast/q/${ZIP}.json`;
+        getWeather(MANURL);
+    } else {
+        console.log('Incorrect Zip Code');
+        return false;
+    }
+
+    form.reset();
+}  
+
+// Google Geolocate API using es6 promises
+function geoLocate() {
+    fetch(GEOURL, {method:'post'})
+        .then(response => response.json())
+        .then(data => {
+            const LAT = data.location.lat;
+            const LON = data.location.lng;
+            let URL = `${WXURL}/api/9dec8582b507efb5/geolookup/forecast/q/${LAT},${LON}.json`;
+            getWeather(URL);
+        })
+        .catch(error => {
+            weather.innerText = error;
+        });
+    }
+
+// call api using es6 promises
+function getWeather(url) {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        weatherUpdate(data);
+      })
+}  
+
+// update screen with api data
+function weatherUpdate(data) {
+    yourLocation.innerText = data.location.city;
+    // Create a new object using the API data
+    var wxForecast = { 
+        myForecast : [
+            {
+                day : data.forecast.simpleforecast.forecastday[0].date.weekday,
+                icon : data.forecast.simpleforecast.forecastday[0].icon_url,
+                conditions : data.forecast.simpleforecast.forecastday[0].conditions,
+                high : data.forecast.simpleforecast.forecastday[0].high.fahrenheit,
+                low : data.forecast.simpleforecast.forecastday[0].low.fahrenheit,
+                forecast : data.forecast.txt_forecast.forecastday[0].fcttext
+            },
+            {
+                day:data.forecast.simpleforecast.forecastday[1].date.weekday,
+                icon:data.forecast.simpleforecast.forecastday[1].icon_url,
+                conditions:data.forecast.simpleforecast.forecastday[1].conditions,
+                high:data.forecast.simpleforecast.forecastday[1].high.fahrenheit,
+                low:data.forecast.simpleforecast.forecastday[1].low.fahrenheit,
+                forecast:data.forecast.txt_forecast.forecastday[2].fcttext
+            },
+            {
+                day:data.forecast.simpleforecast.forecastday[2].date.weekday,
+                icon:data.forecast.simpleforecast.forecastday[2].icon_url,
+                conditions:data.forecast.simpleforecast.forecastday[2].conditions,
+                high:data.forecast.simpleforecast.forecastday[2].high.fahrenheit,
+                low:data.forecast.simpleforecast.forecastday[2].low.fahrenheit,
+                forecast:data.forecast.txt_forecast.forecastday[4].fcttext
+            },
+            {
+                day:data.forecast.simpleforecast.forecastday[3].date.weekday,
+                icon:data.forecast.simpleforecast.forecastday[3].icon_url,
+                conditions:data.forecast.simpleforecast.forecastday[3].conditions,
+                high:data.forecast.simpleforecast.forecastday[3].high.fahrenheit,
+                low:data.forecast.simpleforecast.forecastday[3].low.fahrenheit,
+                forecast:data.forecast.txt_forecast.forecastday[6].fcttext
+            }
+        ]
+    };
+    // Loop through our object and create the DOM elements.
+    for(var i= 0,  len = wxForecast.myForecast.length; i < len; i++) {
+        var weatherContainer = document.createElement('div');
+            weatherContainer.className = "weather-box";
+        var weatherDay = document.createElement('div');
+            weatherDay.className = "weather-day col-md-3";
+        var titleDay = document.createElement('h4');
+            titleDay.className = "weekday";
+        var titleData = document.createTextNode(''+wxForecast.myForecast[i].day+'');
+            titleDay.appendChild(titleData);
+        var iconContainer = document.createElement('div');
+            iconContainer.className = 'icon-container';
+            switch (data.forecast.simpleforecast.forecastday[i].icon) {
+                case 'chanceflurries':
+                case 'chancesnow':
+                case 'flurries':
+                    iconContainer.innerHTML = snowy;
+                    break;
+                case 'chancerain':
+                case 'rain':
+                iconContainer.innerHTML = rainy;
+                    break;
+                case 'chancesleet':
+                case 'sleet':
+                iconContainer.innerHTML = rainy;
+                    break;
+                case 'chancetstorms':
+                case 'tstorms':
+                iconContainer.innerHTML = rainy;
+                    break;
+                case 'clear':
+                iconContainer.innerHTML = rainbow;
+                    break;
+                case 'cloudy':
+                case 'fog':
+                case 'hazy':
+                iconContainer.innerHTML = cloudy;
+                    break;
+                case 'mostlysunny':
+                case 'partlysunny':
+                case 'partlycloudy':
+                case 'mostlycloudly':
+                iconContainer.innerHTML = partlycloudy;
+                    break;
+                case 'sunny':
+                iconContainer.innerHTML = sunny;
+                    break;
+                case 'snow':
+                iconContainer.innerHTML = snowy;
+                    break;
+                default:
+                    iconContainer.innerHTML = snowy;
+            }
+        var subtitle = document.createElement('h5');
+            subtitle.className = 'subtitle';
+        var subtitleData = document.createTextNode(''+wxForecast.myForecast[i].conditions+'');
+            subtitle.appendChild(subtitleData);
+        var temps = document.createElement('p');
+            temps.className = 'temps';
+        var tempsData = document.createTextNode(''+wxForecast.myForecast[i].low+' F / '+wxForecast.myForecast[i].high+' F');
+            temps.appendChild(tempsData);
+        var forecast = document.createElement('p');
+            forecast.className = 'forecast';
+        var forecastData = document.createTextNode(''+wxForecast.myForecast[i].forecast+'');
+            forecast.appendChild(forecastData);
+
+    
+        // Append all DOM elements
+        weatherContainer.appendChild(weatherDay);
+        weatherDay.appendChild(titleDay);
+        weatherDay.appendChild(iconContainer);
+        weatherDay.appendChild(subtitle);
+        weatherDay.appendChild(temps);
+        weatherDay.appendChild(forecast);
+        weatherBoxContainer.appendChild(weatherDay);
+    }
 }
